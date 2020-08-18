7163ae49cafda370fb794632dc70b7ea461b89c5
Jason Ji <uberjason@gmail.com>
2020-08-18 13:20:17 -0400

    Initial Commit

---
+++ NoPopToRootForWatchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoPopToRootForWatchOS WatchKit Extension
+//
+//  Created by Jason Ji on 8/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoPopToRootForWatchOS WatchKit Extension/NoPopToRootForWatchOSApp.swift
@@ -0,0 +1,19 @@
+//
+//  NoPopToRootForWatchOSApp.swift
+//  NoPopToRootForWatchOS WatchKit Extension
+//
+//  Created by Jason Ji on 8/18/20.
+//
+
+import SwiftUI
+
+@main
+struct NoPopToRootForWatchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
