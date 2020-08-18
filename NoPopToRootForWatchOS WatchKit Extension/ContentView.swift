daad8ebefe38091e668d1cd4ab35dc9b9b8737ba
Jason Ji <uberjason@gmail.com>
2020-08-18 13:26:51 -0400

    No way to pop to root on watchOS

--- NoPopToRootForWatchOS WatchKit Extension/ContentView.swift
+++ NoPopToRootForWatchOS WatchKit Extension/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rootIsActive: Bool = false
+    
     var body: some View {
+        NavigationView {
         Text("Hello, World!")
             .padding()
+            NavigationLink("Push Me", destination: DetailView(rootIsActive: $rootIsActive), isActive: $rootIsActive)
+        }
+    }
+}
+    
+struct DetailView: View {
+    @Binding var rootIsActive: Bool
+    
+    var body: some View {
+        VStack {
+            Text("2nd Screen")
+            NavigationLink("Push To 3rd Screen", destination: ThirdView(rootIsActive: $rootIsActive))
+        }
+    }
+}
+
+struct ThirdView: View {
+    @Binding var rootIsActive: Bool
+    
+    var body: some View {
+        VStack {
+            Text("3rd Screen")
+            Button("Try Pop to Root") {
+                rootIsActive.toggle()
+            }
+        }
     }
 }
 
